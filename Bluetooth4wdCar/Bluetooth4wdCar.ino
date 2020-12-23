20943ef667cd672121a06dc0074b9bbc508ef5d5
BerengeaCristian <crisitian.berengea@gmail.com>
2020-12-23 14:48:34 +0200

    commit 2

--- Bluetooth4wdCar/Bluetooth4wdCar.ino
+++ Bluetooth4wdCar/Bluetooth4wdCar.ino
@@ -7,27 +7,249 @@
 #define in3 5
 #define in4 4
 
+#define buzzer 8
+#define tailLights 39
+
+#define trigPin1 40
+#define echoPin1 41
+#define trigPin2 36
+#define echoPin2 37
+
+#define  signalingRight A14
+#define  signalingLeft A15
+
+char command = 'P';
+bool mode = true;
+bool tailLights_state = 1;
+
+int signalingRight_state = -1;
+int signalingLeft_state = -1;
+int x; //signaling pwm
+
+float duration;
+float cm1 = 2000; 
+float cm2 = 2000; 
+
 void setup() {
+  Serial3.begin(9600);
+  Serial3.setTimeout(30);
+  Serial.begin(9600);
   pinMode(enA,OUTPUT);
   pinMode(enB,OUTPUT);
   pinMode(in1,OUTPUT);
   pinMode(in2,OUTPUT);
   pinMode(in3,OUTPUT);
   pinMode(in4,OUTPUT);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,HIGH);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
+
+  pinMode(buzzer,OUTPUT);
+  pinMode(tailLights, OUTPUT);
+
+  pinMode(signalingLeft,OUTPUT);
+  pinMode(signalingRight,OUTPUT);
+
+  pinMode(trigPin1,OUTPUT);
+  pinMode(echoPin1,INPUT);
+  pinMode(trigPin2,OUTPUT);
+  pinMode(echoPin2,INPUT);
 }
 
 void loop() {
-  analogWrite(enB,0);
-  analogWrite(enA,0);
-  delay(5000);
-  analogWrite(enB,128);
-  analogWrite(enA,128);
-  delay(5000);
-  analogWrite(enB,255);
-  analogWrite(enA,255);
-  delay(5000);
+  if(Serial3.available() > 0){ 
+    command = Serial3.read();
+     switch(command){
+      case 'P':  
+      sstop();
+      //V_forward = false;
+      //V_back = false;
+      break;
+     case 'M':  
+      mode = true;
+      sstop();
+      //V_forward = false;
+      //V_back = false;
+     break;
+     case 'N':  
+      mode = false;
+      sstop();
+      //V_forward = false;
+      //V_back = false;
+      break;
+    case 'W':  
+      if(mode) forward(128);
+      else 
+      {
+       // V_forward = true;
+       // V_back = false;
+      }   
+      tailLights_state = 0;
+      break;
+    case 'S':  
+       if(mode) back(255);
+       else 
+       {
+        //V_back = true;
+       // V_forward = false;
+       }
+       tailLights_state = 0;
+      break;
+    case 'A':
+      if(mode) left(255);
+      else 
+      {
+        //V_left = true;
+      }
+      tailLights_state = 0;
+      break;
+    case 'D':
+      if(mode) right(255);
+      else 
+      {
+        //V_right = true;
+      }
+      tailLights_state = 0;
+      break; 
+      case 'B':
+      if(mode) sstop();
+      else
+      {
+       //V_right = false;
+        //V_left = false;
+      }
+      tailLights_state = 1;
+      break;
+      case 'X':
+      signalingRight_state = signalingRight_state * -1;
+      break; 
+      case 'Z':
+      signalingLeft_state = signalingLeft_state * -1;
+      break;  
+    }
+
+    if(command == 'H') tone(buzzer,450);
+    if(command == 'J') noTone(buzzer);
+  }
+
+  if(tailLights_state == 1) analogWrite(tailLights, 255);
+  else  analogWrite(tailLights, 0);
+
+  if(signalingRight_state == 1) analogWrite(signalingRight,x);
+  else analogWrite(signalingRight,0);
+  if(signalingLeft_state == 1) analogWrite(signalingLeft,x);
+  else analogWrite(signalingLeft,0);
+  x+=5;
+  if(x==255) x=0;
+
+  if(command == 'W' || command == 'B' || command == 'P')
+  {
+    ultrasonic1();
+  }else if(command == 'S'){
+    ultrasonic2();
+  }
+
+  if(cm1 <= 15 && command == 'W') 
+    {
+      sstop();
+      //V_forward = false;
+      tailLights_state = 1;
+    }
+   else if(cm2 <= 15 && command == 'S')
+   {
+      sstop();
+      
+      tailLights_state = 1;
+   }
+}
+
+void ultrasonic2()
+{
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin2, LOW);
+  
+
+  duration = pulseIn(echoPin2, HIGH); 
+  cm2 = microsecondsToCentimeters(duration);
+  //if(cm1<=200 || cm2<=200)
+  //{
+    Serial.print(cm2);
+    Serial.println("        cm2");
+  //}else
+  //{
+  //  Serial.println("Out of range      ");
+  //}*/
+}
+void ultrasonic1()
+{
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin1, LOW);
+
+  duration = pulseIn(echoPin1, HIGH); 
+  cm1 = microsecondsToCentimeters(duration);
+  //if(cm1<=200 || cm2<=200)
+  //{
+    Serial.print(cm1);
+    Serial.println("        cm1");
+  //}else
+  //{
+  //  Serial.println("Out of range      ");
+  //}*/
+}
+float microsecondsToCentimeters(float microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+void forward(int speed)
+{
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    analogWrite(enB,speed);
+    analogWrite(enA,speed);
+}
+
+void back(int speed)
+{
+    digitalWrite(in3,HIGH);
+    digitalWrite(in4,LOW);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    analogWrite(enB,speed);
+    analogWrite(enA,speed);
+}
+
+void left(int speed)
+{
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    analogWrite(enB,speed);
+    analogWrite(enA,speed);
+}
+
+void right(int speed)
+{
+    digitalWrite(in3,HIGH);
+    digitalWrite(in4,LOW);
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    analogWrite(enB,speed);
+    analogWrite(enA,speed);
+}
+
+void sstop()
+{
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,LOW);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
 }
